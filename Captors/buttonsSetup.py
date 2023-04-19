9b4f6434141dab9d39e8ebebc7a6c28fe490f385
AntoDB <antonindebreuck@gmail.com>
2023-04-19 11:44:43 +0200

    Update buttonsSetup.py

--- Captors/buttonsSetup.py
+++ Captors/buttonsSetup.py
@@ -2,9 +2,11 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 if __name__ == '__main__':
     while True: # Run forever
-        if GPIO.input(10) == GPIO.HIGH:
+        if GPIO.input(16) == GPIO.HIGH:
             print("Button was pushed!")
+        else:
+            print("Button was NOT pushed!")
