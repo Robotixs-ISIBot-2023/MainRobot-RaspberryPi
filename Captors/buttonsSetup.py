676207d971736fe9a59a08346b80c407b7cbe005
AntoDB <antonindebreuck@gmail.com>
2023-04-19 23:03:35 +0200

    adaptation with variables for the pins

--- Captors/buttonsSetup.py
+++ Captors/buttonsSetup.py
@@ -1,16 +1,19 @@
 import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 
+Pin_Green_Btn = 16
+Pin_Blue_Btn = 18
+
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(Pin_Green_Btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(Pin_Blue_Btn, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 oldGreenButtonPush = False
 oldBlueButtonPush = False
 
 if __name__ == '__main__':
     while True: # Run forever
-        if GPIO.input(16) == GPIO.HIGH:
+        if GPIO.input(Pin_Green_Btn) == GPIO.HIGH:
             if not(oldGreenButtonPush):
                 print("GREEN Button was pushed!")
                 oldGreenButtonPush = True
@@ -19,7 +22,7 @@ if __name__ == '__main__':
                 print("GREEN Button was NOT pushed!")
                 oldGreenButtonPush = False
         
-        if GPIO.input(18) == GPIO.HIGH:
+        if GPIO.input(Pin_Blue_Btn) == GPIO.HIGH:
             if not(oldBlueButtonPush):
                 print("BLUE Button was pushed!")
                 oldBlueButtonPush = True
