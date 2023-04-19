a9db0283457bcf3e00b85725f27e817e29d02a9e
AntoDB <antonindebreuck@gmail.com>
2023-04-19 17:51:37 +0200

    Add blue button

--- Captors/buttonsSetup.py
+++ Captors/buttonsSetup.py
@@ -3,8 +3,10 @@ import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
 GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
 oldGreenButtonPush = False
+oldBlueButtonPush = False
 
 if __name__ == '__main__':
     while True: # Run forever
@@ -16,3 +18,12 @@ if __name__ == '__main__':
             if oldGreenButtonPush:
                 print("GREEN Button was NOT pushed!")
                 oldGreenButtonPush = False
+        
+        if GPIO.input(18) == GPIO.HIGH:
+            if not(oldBlueButtonPush):
+                print("BLUE Button was pushed!")
+                oldBlueButtonPush = True
+        else:
+            if oldBlueButtonPush:
+                print("BLUE Button was NOT pushed!")
+                oldBlueButtonPush = False
