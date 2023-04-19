4065af67c8b33228ec545d29523db6bce48d595a
AntoDB <antonindebreuck@gmail.com>
2023-04-19 11:50:23 +0200

    Only if button's state change

--- Captors/buttonsSetup.py
+++ Captors/buttonsSetup.py
@@ -4,9 +4,15 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
+oldGreenButtonPush = False
+
 if __name__ == '__main__':
     while True: # Run forever
         if GPIO.input(16) == GPIO.HIGH:
-            print("Button was pushed!")
+            if not(oldGreenButtonPush):
+                print("Button was pushed!")
+            oldGreenButtonPush = True
         else:
-            print("Button was NOT pushed!")
+            if oldGreenButtonPush:
+                print("Button was NOT pushed!")
+                oldGreenButtonPush = False
