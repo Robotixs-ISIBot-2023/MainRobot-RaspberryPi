7d5d73d5eca592d8cb08cbe48cae2cc434d5ad6c
AntoDB <antonindebreuck@gmail.com>
2023-04-20 10:15:37 +0200

    Take code on main due to error to debug

--- Captors/gpio_start_setup.py
+++ Captors/gpio_start_setup.py
@@ -1,28 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+ISIBot - Code for the start switch on the Main Robot
+
+Created on Apr 19 2023
+Last modification on Apr 19 2023
+
+Authors : 
+@Sabrine - Sabrine Belhaouar (M2 électronique)
+"""
+
 import RPi.GPIO as GPIO
-import time
 
 GPIO.setmode(GPIO.BCM)
 
-Pin_led = 4
 Pin_switch = 27
 
-GPIO.setup(Pin_led, GPIO.OUT)
 GPIO.setup(Pin_switch, GPIO.IN)
-
-GPIO.output(Pin_led, 0)
 
 old_button_state = 1
 
-while True :
-    
+if __name__ == '__main__':
+    Pin_led = 4
+    GPIO.setup(Pin_led, GPIO.OUT)
+    GPIO.output(Pin_led, 0)
 
-    buttonState = GPIO.input(Pin_switch)
-    
-    """ Pour utiliser le switch de depart en no"""
-    
-    if buttonState == 0 and old_button_state == 1:  
-        GPIO.output(Pin_led,1)  #HIGH
-        print("on")
-        old_button_state = buttonState
+    while True :
+        buttonState = GPIO.input(Pin_switch)
         
+        """ Pour utiliser le switch de depart en no"""
+        
+        if buttonState == 0 and old_button_state == 1:  
+            GPIO.output(Pin_led,1)  #HIGH
+            print("on")
+            old_button_state = buttonState
+            
 GPIO.cleanup()
