18f85f8a2a3e0c5088b8df92348d14e1706fb050
AntoDB <antonindebreuck@gmail.com>
2023-04-20 09:58:23 +0200

    Create gpio_start_setup.py
    Code for start switch on main robot

---
+++ Captors/gpio_start_setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+ISIBot - Code for the start switch on the Main Robot
+
+Created on Apr 19 2023
+Last modification on Apr 19 2023
+
+Authors : 
+@Sabrine - Sabrine Belhaouar (M2 électronique)
+"""
+
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+Pin_switch = 27
+
+GPIO.setup(Pin_switch, GPIO.IN)
+
+old_button_state = 1
+
+if __name__ == '__main__':
+    while True :
+        Pin_led = 4
+        GPIO.setup(Pin_led, GPIO.OUT)
+        GPIO.output(Pin_led, 0)
+
+        buttonState = GPIO.input(Pin_switch)
+        
+        """ Pour utiliser le switch de depart en no"""
+        
+        if buttonState == 0 and old_button_state == 1:  
+            GPIO.output(Pin_led,1)  #HIGH
+            print("on")
+            old_button_state = buttonState
+            
+    GPIO.cleanup()
