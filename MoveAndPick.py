6ca68ff381de99a7551cb19ac2c2b760a7d875b0
AntoDB <antonindebreuck@gmail.com>
2023-04-22 12:36:14 +0200

    Add for execute lidar script

--- MoveAndPick.py
+++ MoveAndPick.py
@@ -24,6 +24,7 @@ from Captors.buttonsSetup import *
 from Captors.gpio_start_setup import *
 
 import time
+import file
 
 #=========================================================================================================#
 
@@ -110,6 +111,8 @@ client.on_message = on_message
 """
     MAIN CODE
 """
+
+exec(open('/home/pi/module_lidar.py').read())
 
 # Wait the start & the team color
 while topics["main_start"] == 0 or topics["teamcolor"] == "null":
