6e33298850c05425e9ae98a6924e11eeaf3dff11
AntoDB <antonindebreuck@gmail.com>
2023-04-22 14:21:18 +0200

    Update gpio_start_setup.py

--- Captors/gpio_start_setup.py
+++ Captors/gpio_start_setup.py
@@ -13,24 +13,24 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
 
-Pin_switch = 13
+Pin_switch = 36
 
 GPIO.setup(Pin_switch, GPIO.IN)
 
 old_button_state = 0
 
 if __name__ == '__main__':
-    Pin_led = 4
-    GPIO.setup(Pin_led, GPIO.OUT)
-    GPIO.output(Pin_led, 0)
+    #Pin_led = 4
+    #GPIO.setup(Pin_led, GPIO.OUT)
+    #GPIO.output(Pin_led, 0)
 
     while True :
         buttonState = GPIO.input(Pin_switch)
-        
+        print(buttonState)
         """ Pour utiliser le switch de depart en no"""
         
         if buttonState == 1 and old_button_state == 0:  
-            GPIO.output(Pin_led,1)  #HIGH
+            #GPIO.output(Pin_led,1)  #HIGH
             print("on")
             old_button_state = buttonState
             
