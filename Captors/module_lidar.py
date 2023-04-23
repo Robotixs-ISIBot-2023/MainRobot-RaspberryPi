e1bce019466f71388d84eef0bc4e9195533b1ddd
AntoDB <antonindebreuck@gmail.com>
2023-04-23 12:13:12 +0200

    Remove back

--- Captors/module_lidar.py
+++ Captors/module_lidar.py
@@ -47,8 +47,13 @@ while True :
                     curr_angle = int(math.degrees(p.angle)+180)
                     dist = (p.range)*254 # 1 ich = 25.4 mm  *0.0254*10*1000 #Il renvoie le range en 1/10 de inches, *0.0254 => to dm, *10 to m, *1000 to mm
                     if dist <= Distance_Securite and dist != 0:
+                        if curr_angle < 46 and curr_angle > 0 or curr_angle < 359 and curr_angle > 315 :
+                            GPIO.output(Pin_Stop,1)
+                    """
+                    if dist <= Distance_Securite and dist != 0:
                         if curr_angle < 225 and curr_angle > 136 or curr_angle < 46 and curr_angle > 0 or curr_angle < 359 and curr_angle > 315 :
                             GPIO.output(Pin_Stop,1)
+                    """
                     """
                     if dist <= Distance_Securite_Arriere and dist != 0:
                         if curr_angle < 225 and curr_angle > 136 :
